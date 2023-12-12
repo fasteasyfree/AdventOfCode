55697fc1cccc962d453559005506780470bd002a
fasteasyfree <fasteasyfree@gmail.com>
2023-12-12 17:36:03 +0000

    Day 11
    on to the actual problem.

--- 2023/11-1.ps1
+++ 2023/11-1.ps1
@@ -1,29 +1,72 @@
 #$InputData = Get-Content $PSScriptRoot\11-1_input.txt
-$InputData = Get-Content $PSScriptRoot\11-1_input_EXAMPLE.txt
+#$InputData = Get-Content $PSScriptRoot\11-1_input_EXAMPLE.txt
+$InputData = @(
+    "...#......",
+    ".......#..",
+    "#.........",
+    "..........",
+    "......#...",
+    ".#........",
+    ".........#",
+    "..........",
+    ".......#..",
+    "#...#....."
+)
 
-$GalaxyMapTemp  = [System.Collections.Generic.List[string]]@()
-$GalaxyMap      = [System.Collections.Generic.List[string]]@()
+# Galaxy map will be a list of lists, containing chars
+# Lists makes inserting easier
+$GalaxyMap = [System.Collections.Generic.List[System.Collections.Generic.List[char]]]@()
 
-# Add the horizontal Cols
-foreach ($Horizontal in $InputData) {
-    $GalaxyMapTemp.Add($Horizontal)
-    if (([System.Collections.Generic.HashSet[char]]($Horizontal.ToCharArray())).count -eq 1) {
-        $GalaxyMapTemp.Add($Horizontal)
+# Take the initial input lines and add them to the list.
+$InputData.Foreach({
+    $GalaxyMap.Add( ($_.ToCharArray()) )
+})
+
+# Create a new row of dots to add
+$NewRow = @((1..10).foreach({[char]'.'}))
+# Add in the horizontal entries
+for ($Row = 0; $Row -lt $GalaxyMap.count; $Row++) {
+
+    # if all there are no hash characters in the current row, add a new one
+    if ( $GalaxyMap[$Row].Contains([char]"#") ) {} 
+    else {
+        # Increment the row number by one to push it to the next line
+        $Row++
+        $GalaxyMap.insert(($Row), $NewRow)
     }
 }
 
-$GalaxyMap = $GalaxyMapTemp
+# Add in the vertical entries. Use the first row to count
+for ($Col = 0; $Col -lt $GalaxyMap[0].count; $Col++) {
 
-# Add the vertical columns
-for ($Col = 0; $Col -lt $GalaxyMapTemp[0].Length; $Col++) {
+    # Create a list of all the characters in the current column
+    $CurrentCol = for ($Row = 0; $Row -lt $GalaxyMap.Count; $Row++) {
+        $GalaxyMap[$Row][$Col]
+    }
 
-    $Vertical = for ($Row=0; $Row -lt $GalaxyMapTemp.count; $Row++) {$GalaxyMapTemp[$Row][$Col]}
-    $Vertical = $Vertical -join ""
-    if (([System.Collections.Generic.HashSet[char]]($Vertical.ToCharArray())).count -eq 1) {
-        $NewCol
-        # One row at a time
-        for ($Row = 0; $Row -lt $GalaxyMapTemp.count; $Row++) {
-            $GalaxyMap[$Row].Insert($Col,".")
+    # Check if that list contains a hash character
+    if ($CurrentCol.contains([char]"#")) {}
+    else {
+        # If it doesn't, push the columns along by one so 
+        # we're adding subsequent ones in the right place
+        $Col++
+        for ($Row = 0; $Row -lt $GalaxyMap.Count; $Row++) {
+            $GalaxyMap[$Row].Insert($Col,'.')
         }
     }
+}
+
+# Now we can find our galaxies
+$GalaxyLocations = [System.Collections.Generic.List[object]]@()
+
+# Map, one line at a time.
+for ($Line = 0; $Line -lt $GalaxyMap.count; $Line++) {
+    # Join the chars up, and search for all instances of '#'. 
+    ((-join $GalaxyMap[$Line]) | Select-String -Pattern "#" -AllMatches).matches.foreach({
+        # Loop through any discoveries and add them.
+        $GalaxyLocations.Add([pscustomobject]@{
+            X = $_.Index
+            Y = $Line
+        })
+    })
 }
